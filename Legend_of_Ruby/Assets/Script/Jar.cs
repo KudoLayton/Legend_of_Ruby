43da5c16678bb17088dc2c609f7aa1da1230c648
Lee H.S <lhslhg@gmail.com>
2015-08-15 04:08:50 +0900

    시야 조정, 항아리 상태 변경

---
+++ Legend_of_Ruby/Assets/Script/Jar.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jar : MonoBehaviour {
+    public int duration;
+    public bool update;
+    public Sprite shrimp;
+    public Sprite poka;
+    public Sprite fruit;
+
+	// Use this for initialization
+	void Start () {
+        update = false;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (update) {
+            switch (duration) {
+                case 3:
+                    gameObject.GetComponent<SpriteRenderer>().sprite = shrimp;
+                    break;
+                case 2:
+                    gameObject.GetComponent<SpriteRenderer>().sprite = poka;
+                    break;
+                case 1:
+                    gameObject.GetComponent<SpriteRenderer>().sprite = fruit;
+                    break;
+                default:
+                    Destroy(gameObject);
+                    break;
+            }
+        }
+	}
+}

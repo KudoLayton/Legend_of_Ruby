8425fec63a039e4b5f7e51b4d525a86c6920c414
cho dong heon <davinnovation@gmail.com>
2015-08-15 00:44:24 +0900

    Error - Mouse Hover

---
+++ Legend_of_Ruby/Assets/Script/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public int jarMax;
+    public GameObject jar;
+    // public Vector2[] startPosition;
+
+	// Use this for initialization
+	void Start () {
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        for (int i = 0; i < jarMax; i++)
+        {
+            transform.position = new Vector2(Random.Range(0, 5), Random.Range(0, 10));
+            Instantiate(jar, transform.position, Quaternion.identity);
+            //Jar temp = (Jar)Instantiate(jar, transform.position, Quaternion.identity);
+            //Instantiate(jar, startPosition[i], Quaternion.identity); 
+            //temp.setID(i);
+        }
+    }
+	
+    // Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Legend_of_Ruby/Assets/Script/Jar.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jar : MonoBehaviour {
+
+    public int durability;
+    public Sprite[] jarImg;
+    public Renderer rend;
+
+    Ray ray;
+    RaycastHit hit;
+
+    // Use this for initializationㅐ
+	void Start () {
+        durability = jarImg.Length;
+        rend = GetComponent<Renderer>();
+    }
+	// Update is called once per frame
+    void Update()
+    {
+        if (durability == 0)
+            Destroy(gameObject);
+        else
+        {
+            ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                Debug.Log(hit.collider.name);
+                if (Input.GetMouseButtonDown(0))
+                    GetAttack();
+            }
+                GetComponent<SpriteRenderer>().sprite = jarImg[jarImg.Length - durability];
+        }
+    }
+    
+
+
+    void GetAttack()
+    {
+        if (durability > 0)
+            durability--;
+    }
+}

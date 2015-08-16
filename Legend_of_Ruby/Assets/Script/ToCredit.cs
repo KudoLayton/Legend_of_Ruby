fdd8822b7b46184592329c02440d58abb827f580
Lee H.S <lhslhg@gmail.com>
2015-08-16 13:25:06 +0900

    Credit 및 발표자료(레알 끝)

---
+++ Legend_of_Ruby/Assets/Script/ToCredit.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToCredit : MonoBehaviour {
+
+    public void Credit() {
+        Application.LoadLevel("credit");
+    }
+}
---
+++ Legend_of_Ruby/Assets/Script/ToTitle.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ToTitle : MonoBehaviour {
+
+    public void Title() {
+        Application.LoadLevel("Title");
+    }
+}
